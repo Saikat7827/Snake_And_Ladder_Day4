ed1cb6dd43378979bfc703389debff234870a308
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-08 20:17:30 +0530

    Welcome text

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("Initial position of player starts from " +position);
+	}
+
+}
