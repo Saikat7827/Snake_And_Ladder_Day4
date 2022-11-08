bc04de2cd1a811e156f6778e0e442009ea38dce1
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-08 20:42:16 +0530

    Added:Playing until the player exact 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,66 +3,44 @@ package com.bridgelabz.snakeladder;
 import java.util.Random;
 
 public class SnakeLadder {
-	static final int snake = 2;
+	static final int noPlay = 0;
 	static final int ladder = 1;
-	static final int Ladder = 1;
-	static final int noplay = 0;
-
-	public static void main(String[] args) 
-	{
+	static final int snake = 2;
+	
+	
+	public static void main(String[] args) {
 		int position = 0;
+		
+		while (position < 100) {
 		Random ran = new Random();
-		int dice = ran.nextInt(6)+1;
-		System.out.println("dice: "+dice);
-		System.out.println("Initial Position is "+position);
-
-		int optionCheck = ran.nextInt(3);
-		System.out.println("optionCheck: "+optionCheck);
-
-		if (optionCheck == ladder) 
-		while (position<100) 
+        int dice = ran.nextInt(6)+1;
+        
+		System.out.println("dice: " + dice);
+        int optionCheck = ran.nextInt(3);
+		System.out.println("optionCheck: " + optionCheck);
+		
+		if ((optionCheck == ladder) && (position + dice) <= 100) 
 		{
 			System.out.println("Ladder");
 			position = position + dice;
 		}
-
-		else if (optionCheck == snake && position > dice) 
+		
+		else if (optionCheck == snake) 
 		{
 			System.out.println("Snake");
 			position = position - dice;
 		}
-		else {
+		
+		else
+		{
 			System.out.println("No Play");
-			Random ran1 = new Random();
-
-			int dice1 = ran1.nextInt(6)+1;
-			System.out.println("Dice Outcome "+dice1);
-
-			int optionCheck1 = ran1.nextInt(3);
-			System.out.println("optionCheck: "+optionCheck1);
-
-
-			if (optionCheck1 == Ladder) 
-			{
-				System.out.println("Ladder");
-				position = position + dice1;
-			}
-			else if (optionCheck1 == snake) 
-			{
-				System.out.println("Snake");
-				position = position - dice1;
-			}
-			else 
-			{
-				System.out.println("NoPlay");
-			}
-			if (position<0) 
-			{
-				position =0;
-			}
-			System.out.println("Position :"+position);
 		}
-		System.out.println("position: "+position);
+		
+		if (position < 0)
+			position = 0;
+		
+		System.out.println("position: " + position);
+		}       
+        
 	}
-
 }
